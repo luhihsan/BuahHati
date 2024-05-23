e7727363eb74deb657974ffb5899b24a9f2cbcbb
wrx_365 <93203737+luhihsan@users.noreply.github.com>
2024-05-23 19:29:22 +0700

    Add XML for Registrasi Anak

---
+++ app/src/main/java/com/modul/buahhati/view/regis_anak/RegistrasiAnakActivity.kt
@@ -0,0 +1,10 @@
+package com.modul.buahhati.view.regis_anak
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RegistrasiAnakActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
